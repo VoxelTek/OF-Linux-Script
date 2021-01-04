88d0c0a7a86cd2d7ec91066b386e7cb163eb8163
VoxelTek <53562267+VoxelTek@users.noreply.github.com>
2021-01-04 18:35:53 +1000

    Fixed (yet again) another issue with the program
    Turns out, yet again, I am stupid.

--- install_debian_based.py
+++ install_debian_based.py
@@ -4,7 +4,7 @@ import time
 a = 0 #Placeholder variable so that 'input' below will work as I want it to.
 
 os.system("sudo apt install subversion") #Installs Subversion, if it isn't already installed.
-os.system("cd ~/.local/share/Steam/steamapps/sourcemods") #Changes the current working directory to the sourcemods/ folder.
+os.system("cd ~/.steam/steam/steamapps/sourcemods") #Changes the current working directory to the sourcemods/ folder.
 
 os.system("svn checkout https://svn.openfortress.fun/svn/open_fortress/ open_fortress") #Clones, or "checkouts" the Open Fortress files from the SVN repository, into "open_fortress" in the sourcemods/ directory.
 
